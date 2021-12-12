c06d0c889a2f487b9eb7d193b8438ede0118b07b
Marcus Hale <marcusxhale1@gmail.com>
2021-12-11 21:44:11 -0800

    This is the correct fix, I just needed a break

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
+const express = require('express');
 const app = express();
 
-app.get('./api/animials', (req, res) => {
-    res.send('Hello')
-});
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
-  });
-
-  const { animals } = require('./data/animals.json')
+});
