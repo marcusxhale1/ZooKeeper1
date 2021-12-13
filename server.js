3b96ce5ada7039c7181c2967f28fb93b18c0c470
Marcus Hale <marcusxhale1@gmail.com>
2021-12-12 16:47:28 -0800

    is this working or no?

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { animals } = require('./data/animals.json');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -94,6 +95,10 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
